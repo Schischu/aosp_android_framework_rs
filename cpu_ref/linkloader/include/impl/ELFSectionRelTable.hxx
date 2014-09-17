a868811c2d61e3ce5d99dc842d29ccce1778c41f
Xiaofei Wan <xiaofei.wan@intel.com>
2014-09-17 16:18:42 +0800

    Implement getMaxNumStubs() for x86 and x86_64.
    Change-Id: I5e7a446d17d3bdf4e93fd8fdaf8fd3ef9c3de571
    Signed-off-by: Xiaofei Wan <xiaofei.wan@intel.com>

--- cpu_ref/linkloader/include/impl/ELFSectionRelTable.hxx
+++ cpu_ref/linkloader/include/impl/ELFSectionRelTable.hxx
@@ -145,8 +145,38 @@ getMaxNumStubs(ELFObjectTy const *obj) const {
     }
 
   case EM_386:
+    {
+      std::set<uint32_t> sym_index_set;
+
+      for (size_t i = 0; i < size(); ++i) {
+        ELFRelocTy *rel = table[i];
+
+        switch (rel->getType()) {
+        case R_386_PC32:
+          sym_index_set.insert(rel->getSymTabIndex());
+          break;
+        }
+      }
+
+      return sym_index_set.size();
+    }
+
   case EM_X86_64:
-    return 0;
+    {
+      std::set<uint32_t> sym_index_set;
+
+      for (size_t i = 0; i < size(); ++i) {
+        ELFRelocTy *rel = table[i];
+
+        switch (rel->getType()) {
+        case R_X86_64_PC32:
+          sym_index_set.insert(rel->getSymTabIndex());
+          break;
+        }
+      }
+
+      return sym_index_set.size();
+    }
 
   default:
     rsl_assert(0 && "Only support ARM, MIPS, X86, and X86_64 relocation.");
