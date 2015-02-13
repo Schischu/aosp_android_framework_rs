8abf9785a857de467f16004b0feffd481a6e0eb3
John Hoford <hoford@google.com>
2015-02-13 15:08:47 -0800

    add better support for landscape mode
    Change-Id: I4d8ab113e69c97695e1a538bbe2a66cbdd15501f

--- java/tests/VrDemo/src/com/example/android/rs/vr/engine/vr.rs
+++ java/tests/VrDemo/src/com/example/android/rs/vr/engine/vr.rs
@@ -31,6 +31,7 @@ rs_matrix4x4 matrix4;
 rs_matrix3x3 matrix3;
 uchar4 base_color;
 static float3 mLight;
+static float rscale;
 
 // material color
 rs_allocation opacity;
@@ -54,9 +55,10 @@ void setup_vectors() {
     base_color.b = 0;
     base_color.a = 255;
     fillJitter();
-    float3 mLightRelitvePos = (float3) {0, 0.7071f, -0.7071f}; // light relitve to camera
+    float3 mLightRelitvePos = (float3) {0.f, 0.7071f, -0.7071f}; // light relitve to camera
     mLight = mLightRelitvePos.x + dx + mLightRelitvePos.y * dy + mLightRelitvePos.z * dz;
     mLight = normalize(mLight);
+    rscale = 1.5f/length(dz);
 }
 
 // old simple version
@@ -173,14 +175,14 @@ uchar4 __attribute__ ((kernel)) draw_z_buffer(float2 in, uint32_t x, uint32_t y)
                 float sdx = rsGetElementAt_float2(zbuff, max(0, (int) x - 1), y).x - in.x;
                 float sdy = rsGetElementAt_float2(zbuff, x, max(0, (int) y - 1)).x - in.x;
                 float dot_prod = sqrt(1 / (1 + (sdy * sdy + sdx * sdx) * zoomFactor));
-                float opf = op / 255.f;
+                float opf = op  * (1/255.f);
                 uchar4 color = rsGetElementAt_uchar4(color_map, intensity * 2);
                 uchar4 mat = rsGetElementAt_uchar4(color_map, intensity * 2 + 1);
                 float4 fcolor = convert_float4(color);;
 
-                float ambient = mat.x / 255.f;
-                float specular = mat.y / 255.f;
-                float diffuse = mat.z / 255.f;
+                float ambient = mat.x * (1/255.f);
+                float specular = mat.y * (1/255.f);
+                float diffuse = mat.z * (1/255.f);
                 float lop = (ambient + diffuse * dot_prod) * light * opf;
                 light -= opf;
                 total_color += fcolor * lop;
@@ -189,7 +191,7 @@ uchar4 __attribute__ ((kernel)) draw_z_buffer(float2 in, uint32_t x, uint32_t y)
             }
         }
     }
-    p += dz * rsRand(2.f);
+    p += dz * rsRand(rscale);
 
     if (light > 0) {
         for (int k = 0; k < izlen - 1; k++) {
@@ -281,9 +283,9 @@ uchar4 __attribute__ ((kernel)) draw_z_buffer(float2 in, uint32_t x, uint32_t y)
                     // Eye point in this space is in the direction (0,0,-1)
                     // Spec * Math.pow(R_z , P) lets use power == 2 (cheap)
 
-                    float ambient = mat.x / 255.f; // ambient
-                    float specular = mat.y / 255.f; // specular not used right now
-                    float diffuse = mat.z / 255.f; // diffuse
+                    float ambient = mat.x * (1/255.f); // ambient
+                    float specular = mat.y * (1/255.f); // specular not used right now
+                    float diffuse = mat.z  * (1/255.f);// diffuse
                     float lop = (ambient + diffuse * dot_prod + specular * pow(spec, 10)) * light * opf;
                     light -= opf;
                     total_color += fcolor * lop;
