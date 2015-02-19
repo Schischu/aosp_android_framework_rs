29f7d5302d395b22400a6be554d73ed71a00b7b4
Pirama Arumuga Nainar <pirama@google.com>
2015-02-19 10:46:29 -0800

    Move bcc::getCommandLine to fw/rs
    libbcc had a utility function to concatenate string arguments.  This
    patch moves that function to frameworks/rs.  The function will no longer
    be needed in libbcc once we move all the checksum/cache invalidation
    logic to frameworks/rs.
    
    Change-Id: I2b1c15c2763d90d7b099e41257bb72f200680943

--- cpu_ref/rsCpuScript.cpp
+++ cpu_ref/rsCpuScript.cpp
@@ -25,7 +25,6 @@
     #include <bcc/BCCContext.h>
     #include <bcc/Config/Config.h>
     #include <bcc/Renderscript/RSCompilerDriver.h>
-    #include <bcc/Renderscript/RSInfo.h>
     #include <bcinfo/MetadataExtractor.h>
     #include <cutils/properties.h>
 
@@ -268,6 +267,18 @@ const char* SharedLibraryUtils::RS_CACHE_DIR = "com.android.renderscript.cache";
 
 #ifndef RS_COMPATIBILITY_LIB
 
+std::string getCommandLine(int argc, const char* const* argv) {
+    std::string s;
+    for (int i = 0; i < argc; i++) {
+        if (i > 0) {
+            s += ' ';
+        }
+        s += argv[i];
+    }
+    return s;
+}
+
+
 bool SharedLibraryUtils::createSharedLibrary(const char *cacheDir, const char *resName) {
     std::string sharedLibName = findSharedObjectName(cacheDir, resName);
     std::string objFileName = cacheDir;
@@ -289,7 +300,7 @@ bool SharedLibraryUtils::createSharedLibrary(const char *cacheDir, const char *r
         nullptr
     };
 
-    std::string cmdLineStr = bcc::getCommandLine(args.size()-1, args.data());
+    std::string cmdLineStr = getCommandLine(args.size()-1, args.data());
 
     pid_t pid = fork();
 
@@ -837,7 +848,7 @@ bool RsdCpuScriptImpl::init(char const *resName, char const *cacheDir,
                         useRSDebugContext, bccPluginName);
     // The last argument of compileArguments ia a nullptr, so remove 1 from the size.
     std::string compileCommandLine =
-                bcc::getCommandLine(compileArguments.size() - 1, compileArguments.data());
+                getCommandLine(compileArguments.size() - 1, compileArguments.data());
 
     if (!is_force_recompile() && !useRSDebugContext) {
         mScriptSO = SharedLibraryUtils::loadSharedLibrary(cacheDir, resName);
